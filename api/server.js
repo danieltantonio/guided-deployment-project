0bf794ce23e42a8c8931200ac951426185491edf
Daniel Antonio <danieltomantonio@gamil.com>
2020-10-01 15:16:21 -0700

    Created routing in server.js

--- api/server.js
+++ api/server.js
@@ -7,4 +7,8 @@ server.get('/', (req,res) => {
     res.status(200).json({ message: 'It works!' });
 });
 
+server.get('/api/test', (req,res) => {
+    res.status(200).json({ message: "Routing works" });
+});
+
 module.exports = server;
